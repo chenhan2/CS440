316b8d25006628006d45b7fc68f8d650a8c39e60
chenhan2 <chenhan2@illinois.edu>
2020-10-06 22:39:56 -0500

    change parameter

--- MP3/naive_bayes.py
+++ MP3/naive_bayes.py
@@ -99,7 +99,7 @@ def calculateUnigramLogProb(sen, pos_prior, logProb):
             logPost_Neg += logProb[0]["UNK"]
     return logPost_Pos, logPost_Neg
 
-def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.08, bigram_smoothing_parameter=0.4, bigram_lambda=0.0001,pos_prior=0.8):
+def bigramBayes(train_set, train_labels, dev_set, unigram_smoothing_parameter=0.1, bigram_smoothing_parameter=1.0 / 2 ** 2, bigram_lambda=0.1,pos_prior=0.8):
     """
     train_set - List of list of words corresponding with each movie review
     example: suppose I had two reviews 'like this movie' and 'i fall asleep' in my training set
