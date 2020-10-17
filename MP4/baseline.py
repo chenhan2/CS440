f9041d91a58f57a4e294157e568e19c4daa7ea97
chenhan2 <chenhan2@illinois.edu>
2020-10-16 23:50:43 -0500

    finish baseline

--- MP4/baseline.py
+++ MP4/baseline.py
@@ -1,7 +1,7 @@
 """
 Part 1: Simple baseline that only uses word statistics to predict tags
 """
-
+from collections import defaultdict
 def baseline(train, test):
     '''
     input:  training data (list of sentences, with tags on the words)
@@ -9,5 +9,29 @@ def baseline(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    
-    return []
+    tagCount = {"UNK":defaultdict(int)}
+    for sen in train:
+        for word, tag in sen:
+            if word not in tagCount:
+                tagCount[word] = defaultdict(int)
+            tagCount[word][tag] += 1
+            tagCount["UNK"][tag] += 1
+    wordTag = {}
+    for word in tagCount:
+        maxTag = None
+        maxCount = 0
+        for tag in tagCount[word]:
+            if tagCount[word][tag] > maxCount:
+                maxCount = tagCount[word][tag]
+                maxTag = tag
+        wordTag[word] = maxTag
+    result = []
+    for sen in test:
+        resultSen = []
+        for word in sen:
+            if word in wordTag:
+                resultSen.append((word, wordTag[word]))
+            else:
+                resultSen.append((word, wordTag["UNK"]))
+        result.append(resultSen)
+    return result
